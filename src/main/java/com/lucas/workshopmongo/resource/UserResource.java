b5c35b053dced6441402811c81ac6593c68db260
Lucas Lima <lucaslimadevx@gmail.com>
2025-07-31 15:14:34 -0300

    Inserção de usuário com POST

--- src/main/java/com/lucas/workshopmongo/resource/UserResource.java
+++ src/main/java/com/lucas/workshopmongo/resource/UserResource.java
@@ -1,13 +1,18 @@
 package com.lucas.workshopmongo.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucas.workshopmongo.domain.User;
 import com.lucas.workshopmongo.dto.UserDTO;
@@ -26,6 +31,23 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 		
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findAll(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
+		
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 		
 	}
 }
